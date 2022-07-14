c3563bd7a3047687969f2d2e27ad3e1914cfbd1e
marianobrc <marianomartinez84@gmail.com>
2022-07-14 19:05:51 -0300

    add the app storing slack messages to installed apps

--- bot/bot-app/bot_app/settings/base.py
+++ bot/bot-app/bot_app/settings/base.py
@@ -46,6 +46,7 @@ LOCAL_APPS = [
     # Project apps go here
     'accounts',
     'events',
+    'slack_messages',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
