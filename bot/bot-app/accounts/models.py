0d45940754594b814e2cc99d70c82fb88c1fa021
marianobrc <marianomartinez84@gmail.com>
2022-08-10 17:18:54 -0300

    Minor style fixes

--- bot/bot-app/accounts/models.py
+++ bot/bot-app/accounts/models.py
@@ -7,7 +7,6 @@ class CustomUser(UUIDModel, AbstractUser):
     # Map the users with Slack users
     slack_user_id = models.CharField(db_index=True, unique=True, max_length=255)
 
-
     class Meta:
         abstract = False
         app_label = 'accounts'
